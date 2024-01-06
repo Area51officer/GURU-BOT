a1f7b422e30eb748eb629256fe22336529017094
Area51officer <53588433+Area51officer@users.noreply.github.com>
2024-01-06 09:36:30 +0530

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,7 +1,7 @@
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`;
+  if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/dQw4w9WgXcQ?si=eS81p1EnLycfxBbe`;
   if (!args[0].match(/youtu/gi)) throw `❎ Verify that it is a YouTube link.`;
 
   m.react(rwait); 
