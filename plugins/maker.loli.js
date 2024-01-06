1149b53bf618c3bef7ddceb6589ccdae2ae2543d
Area51officer <53588433+Area51officer@users.noreply.github.com>
2024-01-06 10:33:41 +0530

    Update maker.loli.js

--- plugins/maker.loli.js
+++ plugins/maker.loli.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/lolice', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/67a2fe0052947c2762f8c.png'),
     }), 'error.png', '*🚔🚨 𝐋𝐎𝐋𝐈𝐂𝐎𝐍𝐒 like you only belong in jail 🚨🚔*', m)
     }
     handler.help = ['lolicon']
