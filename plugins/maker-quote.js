a821f03706ce55c03fa6fbac358050f691513fcc
Area51officer <53588433+Area51officer@users.noreply.github.com>
2024-01-06 10:36:35 +0530

    Update maker-quote.js

--- plugins/maker-quote.js
+++ plugins/maker-quote.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, text }) => {
 
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     if (!(who in global.db.data.users)) throw '✳️ The user is not found in my database';
-     let userPfp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://cdn.jsdelivr.net/gh/Guru322/api@Guru/guru.jpg'); 
+     let userPfp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/67a2fe0052947c2762f8c.png'); 
     let user = global.db.data.users[who];
     let { name } = global.db.data.users[who];
 
