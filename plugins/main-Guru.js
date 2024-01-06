74e4d43000dc40d23622570b62824fcb8926d060
Area51officer <53588433+Area51officer@users.noreply.github.com>
2024-01-06 10:18:31 +0530

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -1,33 +1,21 @@
 let handler = async m => m.reply(`
 
-≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
+≡  *SAM  ┃ ᴮᴼᵀ*   GROUPS
 
 ─────────────
 ▢ Join public bot group and support
 https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
-
-▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
-
-▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
-
-─────────────
-≡ Disabled links? enter here! 
-
-▢ Group WhatsApp 
- https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
 ─────────────
 ▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
+ https://t.me/samkirakun
 
-▢ *YouTube*
-• https://www.youtube.com/@Asliguru
+▢ *Owner*
+• https://wa.me/16106999265
 
 
 `.trim())
 handler.help = ['gpguru']
 handler.tags = ['main']
-handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+handler.command = ['groups', 'groupsam', 'gugp', 'ggp', 'gpguru'] 
 
 export default handler
